403ad36e712ce0a70e6c4e92c6c103555baa0b71
Venera <veneraomurzakkyzy@gmail.com>
2023-06-01 22:27:44 +0600

    feat: add logic to game

--- js/app.js
+++ js/app.js
@@ -12,3 +12,28 @@ const game = document.querySelector("#game"),
 
 minNum.textContent = min;
 maxNum.textContent = max;
+
+guessBtn.addEventListener("click", function () {
+    let guess = parseInt(guessInput.value);
+  
+    if (isNaN(guess) || guess < min || guess > max) {
+      setMessage(`Нужно ввести число от ${min} до ${max}`, "red");
+    }
+  
+    if (guess === winningNum) {
+      guessInput.disabled = true;
+      guessInput.style.border = "2px solid green";
+      setMessage(`Поздравляем вы выиграли `, "green");
+  
+      let newGameBtn = (guessBtn.innerHTML = "Новая игра");
+      if (newGameBtn) {
+        guessBtn.addEventListener("click", function () {
+          location.reload();
+        });
+      }
+    } else {
+      guessInput.style.border = "3px solid red";
+      message.style.color = "red";
+      message.textContent = `Вы не угадали, попыток осталось:  ${guessesLeft}`;
+      guessInput.value = "";
+    }
